902454f69ea5b3c838215726ecba731a2e45bb73
dkrgpisay <dkrgpisayir@gmail.com>
2014-03-20 21:20:00 +0800

    Initial Version
    Version 1

---
+++ images/c_files/resources.js
@@ -0,0 +1,74 @@
+
+
+var LanguageResource =
+{
+	// Common UI
+	uiClose			:"Close",
+	uiShow			:"Show",
+	uiHide			:"Hide",
+    uiShowAll		:"Show all",
+	uiHideAll		:"Hide all",
+	pagingMore      : "Show more",
+	pagePrint: "Print this page",
+	uiNext :"Next",
+	uiPrevious:"Previous",
+
+	// The following are used in the timeago JavaScript
+	timepastDay		: "A day ago",
+	timepastDays	: "{0} days ago",
+	timepastHour	: "About an hour ago",
+	timepastHours	: "About {0} hours ago",
+	timepastMinute	: "About a minute ago",
+	timepastMinutes	: "{0} minutes ago",
+	timepastMonth	: "About a month ago",
+	timepastMonths	: "{0} months ago",
+	timepastSeconds	: "Less than a minute ago",
+	timepastYear	: "About a year ago",
+	timepastYears	: "{0} years ago",
+	timefutureDay	: "A day",
+	timefutureDays	: "{0} days",
+	timefutureHour	: "About an hour",
+	timefutureHours	: "About {0} hours",
+	timefutureMinute: "About a minute",
+	timefutureMinutes: "{0} minutes",
+	timefutureMonth	: "About a month",
+	timefutureMonths: "{0} months",
+	timefutureSeconds: "Less than a minute",
+	timefutureYear	: "About a year",
+	timefutureYears	: "{0} years",
+
+	// Illustrations
+	imageCancelCta  : "Cancel",
+	imageLabel      : "Select a drawing to upload - no photos, just your own artwork please!",
+	imageAdd        : "Add a drawing",
+	imageUploading      : "Your drawing is being uploaded.",
+	imageAddAnother      : "Add another drawing",
+
+	//gifting
+	catHeading: "Choose gift",
+	catSelect: "Select a category",
+
+	// flash
+	flashQuality: "Quality:",
+	flashQualityHigh: "High",
+	flashQualityLow: "Low",
+
+	// sound
+	soundMute: "Mute",
+	soundUnMute: "Unmute",
+
+	//potions
+	potionIndicatorCopy: "This potion requires your attention",
+
+	end: true
+		
+}
+
+var UrlResource =
+{
+	// Error URLs
+	error404: "/en-us/error/not-found/",
+	error500: "/en-us/error/problem",
+
+	end: true
+}
